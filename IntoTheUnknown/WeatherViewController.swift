e0c5e056d3d3afc84967f277c5ac2ad5d4cb90c1
Pulavarthi Shravya <S10205493@CONNECT.NP.EDU.SG>
2022-01-18 10:35:13 +0800

    added in view controller file to display weather information

---
+++ IntoTheUnknown/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  IntoTheUnknown
+//
+//  Created by Pulavarthi Shravya on 18/1/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
