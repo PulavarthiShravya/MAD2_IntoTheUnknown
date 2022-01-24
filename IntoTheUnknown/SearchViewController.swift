db5c24e9524338304e535c267e04a11846d4ebf1
Pulavarthi Shravya <S10205493@CONNECT.NP.EDU.SG>
2022-01-24 12:56:48 +0800

    added in view controller for search function

---
+++ IntoTheUnknown/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  IntoTheUnknown
+//
+//  Created by Pulavarthi Shravya on 24/1/22.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
